116c10ef4cf4e9f4949431831679a626c12611ea
Bismarck Villca <bismarck.villca@gmail.com>
2025-06-02 22:34:51 -0400

    fix: update GitHub Pages environment variable check in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GH_PAGES === 'true';
+const isGithubPages = 'true';
 const nextConfig = {
   output: 'export',
   basePath: isGithubPages ? '/pass-generator>' : '',
