a7270bb095f22ff896821a96bc2b7c655392e84f
Bismarck Villca <bismarck.villca@gmail.com>
2025-06-02 22:14:24 -0400

    feat: configure Next.js for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
 const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? '/pass-generator>' : '',
+  assetPrefix: isGithubPages ? '/pass-generator/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
