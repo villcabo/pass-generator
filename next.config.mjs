d209d02fd3f3c1e3ce07810c89586029c173dafa
Bismarck Villca <bismarck.villca@gmail.com>
2025-06-02 22:28:40 -0400

    fix: correct environment variable name for GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const isGithubPages = process.env.GH_PAGES === 'true';
 const nextConfig = {
   output: 'export',
   basePath: isGithubPages ? '/pass-generator>' : '',
